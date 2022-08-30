b394a350344e05e317fc0ad37b4b8e3414b7a17e
humoyun-webs <humoyuneshpolatov5@gmail.com>
2022-08-30 12:41:41 -0800

    Snap

---
+++ script.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector(".menu")
+const menu_section = document.querySelector(".menu_section")
+
+const fuction = menu.addEventListener("click", () =>{
+    menu.value = menu_section
+    
+})
